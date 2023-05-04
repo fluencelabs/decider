413bb225b6dd4bf4e1087a9a37b5026a57844303
Alexey Proshutinskiy <alexey.prosh@fluence.one>
2023-05-04 13:00:34 +0400

    feat(builtin-package): use new blueprint

--- builtin-package/package.sh
+++ builtin-package/package.sh
@@ -3,34 +3,55 @@ set -o pipefail -o nounset -o errexit
 
 # set current working directory to script directory to run script from everywhere
 cd "$(dirname "$0")"
-SCRIPT_DIR="$(pwd)"
+PACKAGE_DIR="$(pwd)/../connector"
+
+(
+    rm -rf $PACKAGE_DIR
+    mkdir -p $PACKAGE_DIR
+)
+
+(
+
+    echo "*** build fluence_aurora_connector ***"
+    cd ..
+    fluence build
+)
+
+(
+    echo "*** copy wasm files ***"
+    cd ..
+    cp target/wasm32-wasi/release/fluence_aurora_connector.wasm "$PACKAGE_DIR"
+    cp target/wasm32-wasi/release/curl_adapter.wasm "$PACKAGE_DIR"
+)
 
 (
     echo "*** compile spell scripts ***"
     cd ..
     ./scripts/compile.sh example/periodic.json example/decider_init_args.json example/periodic.json
-    cp decider.json builtin-package/on_start.json
+    cp decider.json "$PACKAGE_DIR"/on_start.json
 )
 
 (
-    echo "*** build fluence_aurora_connector ***"
-    cd ..
-    fluence build
+    echo "*** copy on_start script ***"
+    cp on_start.air "$PACKAGE_DIR"
 )
-(
-    echo "*** copy wasm files ***"
-    cd ..
-    pwd
-    cp target/wasm32-wasi/release/fluence_aurora_connector.wasm "$SCRIPT_DIR"
-    cp target/wasm32-wasi/release/curl_adapter.wasm "$SCRIPT_DIR"
-)
+
+CONNECTOR_CID=$(ipfs add -q --only-hash --cid-version=1 --chunker=size-262144 $PACKAGE_DIR/fluence_aurora_connector.wasm)
+CURL_CID=$(ipfs add -q --only-hash --cid-version=1 --chunker=size-262144 $PACKAGE_DIR/curl_adapter.wasm)
+mv $PACKAGE_DIR/fluence_aurora_connector.wasm "$PACKAGE_DIR"/"$CONNECTOR_CID".wasm
+mv $PACKAGE_DIR/curl_adapter.wasm "$PACKAGE_DIR"/"$CURL_CID".wasm
+cp fluence_aurora_connector_config.json "$PACKAGE_DIR"/"$CONNECTOR_CID"_config.json
+cp curl_adapter_config.json "$PACKAGE_DIR"/"$CURL_CID"_config.json
+
+# write blueprint.json
+echo "{}" | jq --arg trust_graph_cid "$CONNECTOR_CID" --arg sqlite_cid "$CURL_CID" '{"name": "connector", "dependencies":[{"/":$sqlite_cid},{"/":$trust_graph_cid}]}' > "$PACKAGE_DIR/blueprint.json"
 
 (
     echo "*** create builtin distribution package ***"
     cd ..
-    cp -rf builtin-package connector
-    tar --exclude="package.sh" -f connector.tar.gz -zcv ./connector
-    rm -rf connector
+
+    tar -f connector.tar.gz -zcv ./connector
+
 )
 
 echo "*** done ***"
