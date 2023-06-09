f240ca4fc1f63e36e7a85b72ced098dc1fe28ed4
Maria Kuklina <101095419+kmd-fl@users.noreply.github.com>
2023-06-09 12:05:27 +0200

    feat: add distro crate [fixes NET-464]  (#51)
    * add distro crate
    * Publish crate on release
    
    Co-authored-by: Anatoly Laskaris <github_me@nahsi.dev>

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+
+# set current working directory to script directory to run script from everywhere
+cd "$(dirname "$0")"
+
+DISTRO_DIR=src/distro/decider-spell
+
+mkdir -p "$DISTRO_DIR"
+
+# compile aqua file
+fluence aqua -i src/aqua/worker.aqua -o "$DISTRO_DIR" --air
+fluence aqua -i src/aqua/decider.aqua -o "$DISTRO_DIR" --air
+
+# compile connector
+fluence build
+
+cp target/wasm32-wasi/release/fluence_aurora_connector.wasm src/distro/decider-spell/
+cp target/wasm32-wasi/release/curl_adapter.wasm src/distro/decider-spell/
+cp example/Config.toml src/distro/decider-spell/
+
+# compile distro
+cd src/distro
+cargo build
