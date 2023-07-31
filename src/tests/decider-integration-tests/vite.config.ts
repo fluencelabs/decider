aedc5f03351a0f892d4cd7f55b30da1183e558ff
folex <0xdxdy@gmail.com>
2023-07-31 12:52:41 -0500

    feat(tests): remove js tests

--- src/tests/decider-integration-tests/vite.config.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  test: {
-    // Default: ['**/*.{test,spec}.?(c|m)[jt]s?(x)']
-    // ...
-  },
-})
