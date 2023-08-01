bffe28f2a2d1af2e758e25e544b1bb9a94cb8ed1
folex <0xdxdy@gmail.com>
2023-08-01 13:05:28 -0500

    feat(match): log parsing works

--- src/services/fluence-aurora-connector/modules/fluence_aurora_connector/src/chain/deal_matched.rs
+++ src/services/fluence-aurora-connector/modules/fluence_aurora_connector/src/chain/deal_matched.rs
@@ -120,14 +120,22 @@ impl ChainEvent<Match> for DealMatched {
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+
+    use ethabi::Token;
+    use libp2p_identity::PeerId;
+
     use crate::chain::chain_data::ChainData;
     use crate::chain::deal_matched::{parse_peer_id, DealMatched, Match};
     use crate::chain::log::{parse_log, Log};
     use crate::jsonrpc::JsonRpcResp;
-    use ethabi::Token;
 
     #[test]
     fn topic() {
+        // topics: [
+        // '0x55e61a24ecdae954582245e5e611fb06905d6af967334fff4db72793bebc72a9',
+        // '0x7a82a5feefcaad4a89c689412031e5f87c02b29e3fced583be5f05c7077354b7'
+        // ],
         assert_eq!(
             Match::topic(),
             String::from("0x55e61a24ecdae954582245e5e611fb06905d6af967334fff4db72793bebc72a9")
@@ -168,14 +176,28 @@ mod tests {
                     "removed": false,
                     "logIndex": "0xb",
                     "transactionIndex": "0x0",
-                    "transactionHash": "0x1a7122fa7501f09f19f29451548e88adf7ec88c99d34b4abdd09b27dfdbd74f1",
-                    "blockHash": "0x1c6808f9f4f99bdad9a63601e07230b84effaec5aba724963ef17651131cf75d",
-                    "blockNumber": "0x4e",
-                    "address": "0x6328bb918a01603adc91eae689b848a9ecaef26d",
-                    "data": "0x00000000000000000000000099e28f59ddfe14ff4e598a3ba3928bbf87b3f2b30000000000000000000000000000000000000000000000000000000000000003000000000000000000000000000000000000000000000000000000000000004d0155122000000000000000000000000000000000000000000000000000000000ae5c519332925f31f747a4edd958fb5b0791b10383ec6d5e77e2264f211e09e3",
+                    "transactionHash": "0xe3943cc5057c8ed33ec9a6891421b367d0f8179b167559ca6e1dae9992941003",
+                    "blockHash": "0xa26b32fbefcf53e5484c0325fd6da72ee03c7198f1c32b4f8b4582b93525837b",
+                    "blockNumber": "0x51",
+                    "address": "0xb971228a3af887c8c50e7ab946df9def0d12cab2",
+                    "data": "0x000000000000000000000000ffa0611a099ab68ad7c3c67b4ca5bbbee7a58b9900000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000000500155122000000000000000000000000000000000000000000000000000000000ae5c519332925f31f747a4edd958fb5b0791b10383ec6d5e77e2264f211e09e300000000000000000000000000000000000000000000000000000000000000036c9d5e8bcc73a422dd6f968f13cd6fc92ccd5609b455cf2c7978cbc694297853fef3b95696986bf289166835e05f723f0fdea97d2bc5fea0ebbbf87b6a866cfa5a5a0f4fa4d41a4f976e799895cce944d5080041dba7d528d30e81c67973bac3",
                     "topics": [
-                        "0x8a2ecab128faa476aff507c7f34da3348b5c56e4a0401825f6919b4cc7b249f1",
-                        "0x0000000000000000000000006f10e8209296ea9e556f80b0ff545d8175f271d0"
+                        "0x55e61a24ecdae954582245e5e611fb06905d6af967334fff4db72793bebc72a9",
+                        "0x7a82a5feefcaad4a89c689412031e5f87c02b29e3fced583be5f05c7077354b7"
+                    ]
+                },
+                {
+                    "removed": false,
+                    "logIndex": "0xb",
+                    "transactionIndex": "0x0",
+                    "transactionHash": "0x093f57ec0df3420f3c8a52ee90fa9ef05aed9827fa05ba6e997bdd4b1b982189",
+                    "blockHash": "0xd2f21035758026e7f0be21c13278b1d4f993b6d75647b0c29d431a4f271ccfd0",
+                    "blockNumber": "0x57",
+                    "address": "0xb971228a3af887c8c50e7ab946df9def0d12cab2",
+                    "data": "0x00000000000000000000000067b2ad3866429282e16e55b715d12a77f85b7ce800000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000000560155122000000000000000000000000000000000000000000000000000000000ae5c519332925f31f747a4edd958fb5b0791b10383ec6d5e77e2264f211e09e300000000000000000000000000000000000000000000000000000000000000036c9d5e8bcc73a422dd6f968f13cd6fc92ccd5609b455cf2c7978cbc694297853fef3b95696986bf289166835e05f723f0fdea97d2bc5fea0ebbbf87b6a866cfa5a5a0f4fa4d41a4f976e799895cce944d5080041dba7d528d30e81c67973bac3",
+                    "topics": [
+                        "0x55e61a24ecdae954582245e5e611fb06905d6af967334fff4db72793bebc72a9",
+                        "0x7a82a5feefcaad4a89c689412031e5f87c02b29e3fced583be5f05c7077354b7"
                     ]
                 }
             ]
@@ -185,18 +207,39 @@ mod tests {
         let logs: JsonRpcResp<Vec<Log>> = serde_json::from_str(jsonrpc).expect("invalid jsonrpc");
         let log = logs.result[0].clone();
         let m = parse_log::<Match, DealMatched>(log).expect("error parsing Match from log");
-        assert_eq!(m.block_number, "0x4e");
+        assert_eq!(m.block_number, "0x51");
         let m = m.info;
         assert_eq!(
-            m.compute_peer.to_string(),
-            "6f10e8209296ea9e556f80b0ff545d8175f271d0"
+            m.compute_peer,
+            PeerId::from_str("12D3KooWJ4bTHirdTFNZpCS72TAzwtdmavTBkkEXtzo6wHL25CtE")
+                .expect("parse peer id")
         );
         assert_eq!(
             m.deal_id.to_lowercase(),
-            "99e28f59ddfe14ff4e598a3ba3928bbf87b3f2b3"
+            "0xFfA0611a099AB68AD7C3C67B4cA5bbBEE7a58B99".to_lowercase()
         );
-        assert_eq!(m.pat_ids.len(), 33);
-        assert_eq!(m.deal_creation_block.to_eth().as_u32(), 77);
+        assert_eq!(m.pat_ids.len(), 3);
+        assert_eq!(m.deal_creation_block.to_eth().as_u32(), 80);
+        assert_eq!(
+            m.app_cid.to_string(),
+            "bafkreifolrizgmusl4y7or5e5xmvr623a6i3ca4d5rwv457cezhschqj4m"
+        );
+
+        let log = logs.result[1].clone();
+        let m = parse_log::<Match, DealMatched>(log).expect("error parsing Match from log");
+        assert_eq!(m.block_number, "0x57");
+        let m = m.info;
+        assert_eq!(
+            m.compute_peer,
+            PeerId::from_str("12D3KooWJ4bTHirdTFNZpCS72TAzwtdmavTBkkEXtzo6wHL25CtE")
+                .expect("parse peer id")
+        );
+        assert_eq!(
+            m.deal_id.to_lowercase(),
+            "0x67b2AD3866429282e16e55B715d12A77F85B7CE8".to_lowercase()
+        );
+        assert_eq!(m.pat_ids.len(), 3);
+        assert_eq!(m.deal_creation_block.to_eth().as_u32(), 86);
         assert_eq!(
             m.app_cid.to_string(),
             "bafkreifolrizgmusl4y7or5e5xmvr623a6i3ca4d5rwv457cezhschqj4m"
