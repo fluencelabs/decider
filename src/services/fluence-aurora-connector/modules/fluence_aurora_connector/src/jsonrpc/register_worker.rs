02acb7b53992a5cda2862a129dab21c74333e9a3
folex <0xdxdy@gmail.com>
2023-08-07 13:33:48 -0500

    chore(worker): refactor

--- src/services/fluence-aurora-connector/modules/fluence_aurora_connector/src/jsonrpc/register_worker.rs
+++ src/services/fluence-aurora-connector/modules/fluence_aurora_connector/src/jsonrpc/register_worker.rs
@@ -1,47 +1,72 @@
 use marine_rs_sdk::marine;
 
+use crate::jsonrpc::register_worker::RegisterWorkerError::EncodeArgument;
+use ethabi::{Function, Param, ParamType, StateMutability, Token};
+use hex::FromHexError;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum RegisterWorkerError {
+    #[error("error encoding function argument {1}: {0:?}")]
+    EncodeArgument(FromHexError, &'static str),
+    #[error("error encoding function inputs: {0:?}")]
+    EncodeInput(#[from] ethabi::Error),
+}
+
 #[marine]
-pub fn register_worker() {}
+pub fn register_worker() {
+    unimplemented!()
+}
+
+fn function() -> Function {
+    #[allow(deprecated)]
+    Function {
+        name: String::from("setWorker"),
+        inputs: vec![
+            Param {
+                name: String::from("patId"),
+                kind: ParamType::FixedBytes(32),
+                internal_type: None,
+            },
+            Param {
+                name: String::from("workerId"),
+                kind: ParamType::FixedBytes(32),
+                internal_type: None,
+            },
+        ],
+        outputs: vec![],
+        constant: None,
+        state_mutability: StateMutability::NonPayable,
+    }
+}
+
+fn encode_call(pat_id: &str, worker_id: &str) -> Result<String, RegisterWorkerError> {
+    let pat_id = hex::decode(pat_id).map_err(|e| EncodeArgument(e, "pat_id"))?;
+    let pat_id = Token::FixedBytes(pat_id);
+
+    let worker_id = hex::decode(worker_id).map_err(|e| EncodeArgument(e, "worker_id"))?;
+    let worker_id = Token::FixedBytes(worker_id);
+
+    let input = function().encode_input(&[pat_id, worker_id])?;
+    let input = hex::encode(input);
+    Ok(input)
+}
 
 #[cfg(test)]
 mod tests {
-    use ethabi::{Function, Param, ParamType, StateMutability, Token};
+    use crate::jsonrpc::register_worker::{encode_call, function};
 
     #[test]
     fn gen_tx() {
         // function setWorker(bytes32 patId, bytes32 workerId) external
-        #[allow(deprecated)]
-        let f = Function {
-            name: "setWorker".into(),
-            inputs: vec![
-                Param {
-                    name: "patId".to_string(),
-                    kind: ParamType::FixedBytes(32),
-                    internal_type: None,
-                },
-                Param {
-                    name: "workerId".to_string(),
-                    kind: ParamType::FixedBytes(32),
-                    internal_type: None,
-                },
-            ],
-            outputs: vec![],
-            constant: None,
-            state_mutability: StateMutability::NonPayable,
-        };
-
+        let f = function();
         assert_eq!(f.signature(), "setWorker(bytes32,bytes32)");
         let signature_hex = hex::encode(f.short_signature());
         assert_eq!(signature_hex, "d5053ab0");
 
         let pat_id = "e532c726aa9c2f223fb21b5a488f874583e809257685ac3c40c9e0f7c89c082e";
-        let pat_id = hex::decode(pat_id).expect("decode pat_id from hex");
         let worker_id = "529d4dabfa72abfd83c48adca7a2d49a921fa7351689d12e2a6c68375052f0b5";
-        let worker_id = hex::decode(worker_id).expect("decode worker_id from hex");
-        let input = f
-            .encode_input(&[Token::FixedBytes(pat_id), Token::FixedBytes(worker_id)])
-            .expect("encode input");
-        let input = hex::encode(input);
+        let input = encode_call(pat_id, worker_id).expect("encode call");
         assert_eq!(input, "d5053ab0e532c726aa9c2f223fb21b5a488f874583e809257685ac3c40c9e0f7c89c082e529d4dabfa72abfd83c48adca7a2d49a921fa7351689d12e2a6c68375052f0b5");
     }
 }
