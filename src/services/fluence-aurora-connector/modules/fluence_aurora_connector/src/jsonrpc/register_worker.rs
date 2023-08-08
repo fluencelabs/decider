b9c702b268330f5937769ac2e39ddb01a6d9e989
folex <0xdxdy@gmail.com>
2023-08-08 07:24:48 -0500

    chore(worker): gen tx and sign

--- src/services/fluence-aurora-connector/modules/fluence_aurora_connector/src/jsonrpc/register_worker.rs
+++ src/services/fluence-aurora-connector/modules/fluence_aurora_connector/src/jsonrpc/register_worker.rs
@@ -40,7 +40,7 @@ fn function() -> Function {
     }
 }
 
-fn encode_call(pat_id: &str, worker_id: &str) -> Result<String, RegisterWorkerError> {
+fn encode_call(pat_id: &str, worker_id: &str) -> Result<Vec<u8>, RegisterWorkerError> {
     let pat_id = hex::decode(pat_id).map_err(|e| EncodeArgument(e, "pat_id"))?;
     let pat_id = Token::FixedBytes(pat_id);
 
@@ -48,16 +48,17 @@ fn encode_call(pat_id: &str, worker_id: &str) -> Result<String, RegisterWorkerEr
     let worker_id = Token::FixedBytes(worker_id);
 
     let input = function().encode_input(&[pat_id, worker_id])?;
-    let input = hex::encode(input);
     Ok(input)
 }
 
 #[cfg(test)]
 mod tests {
     use crate::jsonrpc::register_worker::{encode_call, function};
+    use ethereum_tx_sign::Transaction;
+    use std::convert::TryInto;
 
     #[test]
-    fn gen_tx() {
+    fn gen_call() {
         // function setWorker(bytes32 patId, bytes32 workerId) external
         let f = function();
         assert_eq!(f.signature(), "setWorker(bytes32,bytes32)");
@@ -67,6 +68,37 @@ mod tests {
         let pat_id = "e532c726aa9c2f223fb21b5a488f874583e809257685ac3c40c9e0f7c89c082e";
         let worker_id = "529d4dabfa72abfd83c48adca7a2d49a921fa7351689d12e2a6c68375052f0b5";
         let input = encode_call(pat_id, worker_id).expect("encode call");
+        let input = hex::encode(input);
         assert_eq!(input, "d5053ab0e532c726aa9c2f223fb21b5a488f874583e809257685ac3c40c9e0f7c89c082e529d4dabfa72abfd83c48adca7a2d49a921fa7351689d12e2a6c68375052f0b5");
+    }
+
+    #[test]
+    fn gen_tx() {
+        use ethereum_tx_sign::LegacyTransaction;
+
+        let pat_id = "e532c726aa9c2f223fb21b5a488f874583e809257685ac3c40c9e0f7c89c082e";
+        let worker_id = "529d4dabfa72abfd83c48adca7a2d49a921fa7351689d12e2a6c68375052f0b5";
+        let input = encode_call(pat_id, worker_id).expect("encode call");
+
+        let address = hex::decode("908aEBfb6051Bca6d1e684586d7760e53C4c736C")
+            .expect("decode Matcher addr from hex");
+        let address = address.try_into().expect("convert address to fixed array");
+
+        let tx = LegacyTransaction {
+            chain: 31337,
+            nonce: 1,
+            to: Some(address),
+            value: 0,
+            gas_price: 50000,
+            gas: 210000,
+            data: input,
+        };
+
+        let private_key = "bb3457514f768615c8bc4061c7e47f817c8a570c5c3537479639d4fad052a98a";
+        let private_key = hex::decode(private_key).expect("decode private key from hex");
+        assert_eq!(private_key.len(), 32);
+        let ecdsa = tx.ecdsa(&private_key).expect("calculate signature");
+        let tx_bytes = tx.sign(&ecdsa);
+        println!("tx_bytes 0x{}", hex::encode(tx_bytes));
     }
 }
