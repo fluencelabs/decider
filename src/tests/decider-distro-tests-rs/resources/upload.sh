fefa944506a0fa120f69954cb2e677b1130f818e
Maria Kuklina <maria@fluence.one>
2023-10-10 15:20:55 +0200

    debug info

--- src/tests/decider-distro-tests-rs/resources/upload.sh
+++ src/tests/decider-distro-tests-rs/resources/upload.sh
@@ -8,6 +8,7 @@ do
     cd "$proj"
     for fl in $(find . -mindepth 1 -type f)
     do
+        echo "uploading file $fl"
     	ipfs add --cid-version 1 --hash sha2-256 -r "$fl" --api /ip4/127.0.0.1/tcp/5001
     done
     cd -
@@ -15,4 +16,4 @@ done
 
 
 echo "try get"
-ipfs get -o /tmp/test.wasm bafkreici665k2iypfxyxgc7zh6wyho6gqogyald7zz3k6tsjzxcuhgpx7u --api /ip4/127.0.0.1/tcp/5001 --timeout 60s
+time ipfs --timeout 60s get -o /tmp/test.wasm bafkreici665k2iypfxyxgc7zh6wyho6gqogyald7zz3k6tsjzxcuhgpx7u --api /ip4/127.0.0.1/tcp/5001
