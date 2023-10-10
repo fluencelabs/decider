46a61b4913b21764f0db8dba77ea1024e3a8e1cb
Maria Kuklina <maria@fluence.one>
2023-10-10 14:53:20 +0200

    debug info

--- src/tests/decider-distro-tests-rs/resources/upload.sh
+++ src/tests/decider-distro-tests-rs/resources/upload.sh
@@ -8,13 +8,20 @@ do
 	ipfs add --cid-version 1 --hash sha2-256 -r "$proj" --api /ip4/127.0.0.1/tcp/5001
 done
 
+echo "DONE"
+
+API=/ip4/127.0.0.1/tcp/5001
+
+ipfs ls bafybeigkrlu4isbagglvc5lhcuxhq6i5o6hjyvnfqcjct72hspbydupxde --api $API --timeout 10s
+
+
 echo "check uploaded file: cat"
 ipfs cat bafkreifolrizgmusl4y7or5e5xmvr623a6i3ca4d5rwv457cezhschqj4m --api /ip4/127.0.0.1/tcp/5001 --timeout 10s
 
-echo "\n\ncheck uploaded file: get"
+echo -n "\n\ncheck uploaded file: get"
 
 ipfs get -o /tmp/test.json bafkreifolrizgmusl4y7or5e5xmvr623a6i3ca4d5rwv457cezhschqj4m --api /ip4/127.0.0.1/tcp/5001 --timeout 10s
 
-echo "\n\ncheck uploaded file: get"
+echo -n "\n\ncheck uploaded file: get"
 
 ipfs get -o /tmp/test.wasm bafkreici665k2iypfxyxgc7zh6wyho6gqogyald7zz3k6tsjzxcuhgpx7u --api /ip4/127.0.0.1/tcp/5001 --timeout 60s
