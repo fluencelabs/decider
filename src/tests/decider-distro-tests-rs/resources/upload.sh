2918875bb03617badf99ae85caa70597ea3b7783
Maria Kuklina <maria@fluence.one>
2023-10-10 11:50:12 +0200

    try fix ci

--- src/tests/decider-distro-tests-rs/resources/upload.sh
+++ src/tests/decider-distro-tests-rs/resources/upload.sh
@@ -5,5 +5,5 @@ cd $(dirname "$0")
 
 for proj in $(find . -mindepth 1 -type d)
 do
-	ipfs add --cid-version 1 --hash sha2-256 -r "$proj"
+	ipfs add --cid-version 1 --hash sha2-256 -r "$proj" --api /ip4/127.0.0.1/tcp/5001
 done
