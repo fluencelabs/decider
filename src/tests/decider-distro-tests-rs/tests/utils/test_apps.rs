f2882cd83cf01151c8412332071cbd0467fc0584
Maria Kuklina <101095419+kmd-fl@users.noreply.github.com>
2024-02-26 13:14:08 +0100

    feat: update to new Installation Spell with new effector restrictions (#248)

--- src/tests/decider-distro-tests-rs/tests/utils/test_apps.rs
+++ src/tests/decider-distro-tests-rs/tests/utils/test_apps.rs
@@ -1,5 +1,7 @@
 use crate::utils::to_hex;
+use cid::Cid;
 use serde_json::{json, Value};
+use std::str::FromStr;
 
 pub struct TestApp {
     pub cid: String,
@@ -10,13 +12,23 @@ impl TestApp {
     // Predefined url_downloader app
     pub fn test_app1() -> Self {
         Self {
-            cid: "bafkreifolrizgmusl4y7or5e5xmvr623a6i3ca4d5rwv457cezhschqj4m".to_string(),
+            cid: "bafkreiekvwp2w7t7vw4jzjq4s4n4wc323c6dnexmy4axh6c7tiza5wxzm4".to_string(),
             services_names: vec!["url_downloader".to_string()],
         }
     }
 
+    fn encoded_cid(&self) -> String {
+        let x = Cid::from_str(&self.cid).unwrap();
+        let bts = x.to_bytes();
+        let prefix = hex::encode(bts[0..4].to_vec());
+        let hash = hex::encode(bts[4..].to_vec());
+        format!("{prefix}00000000000000000000000000000000000000000000000000000000{hash}")
+    }
+
     pub fn log_test_app1(deal_id: &str, block: u32, host_topic: &str) -> Value {
-        // Encoded CID (url-downloader): bafkreifolrizgmusl4y7or5e5xmvr623a6i3ca4d5rwv457cezhschqj4m
+        let test_app = Self::test_app1();
+        let app_cid = test_app.encoded_cid();
+        // Encoded CID (url-downloader)
         // TODO: generate this on fly
         json!(
             {
@@ -27,7 +39,7 @@ impl TestApp {
                 "blockHash": "0x2",
                 "blockNumber": to_hex(block),
                 "address": "0xb971228a3af887c8c50e7ab946df9def0d12cab2",
-                "data": format!("0x000000000000000000000000{deal_id}00000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000000500155122000000000000000000000000000000000000000000000000000000000ae5c519332925f31f747a4edd958fb5b0791b10383ec6d5e77e2264f211e09e300000000000000000000000000000000000000000000000000000000000000036c9d5e8bcc73a422dd6f968f13cd6fc92ccd5609b455cf2c7978cbc694297853fef3b95696986bf289166835e05f723f0fdea97d2bc5fea0ebbbf87b6a866cfa5a5a0f4fa4d41a4f976e799895cce944d5080041dba7d528d30e81c67973bac3"),
+                "data": format!("0x000000000000000000000000{deal_id}00000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000050{app_cid}00000000000000000000000000000000000000000000000000000000000000036c9d5e8bcc73a422dd6f968f13cd6fc92ccd5609b455cf2c7978cbc694297853fef3b95696986bf289166835e05f723f0fdea97d2bc5fea0ebbbf87b6a866cfa5a5a0f4fa4d41a4f976e799895cce944d5080041dba7d528d30e81c67973bac3"),
                 "topics": [
                     "0x1c13422d2375fe8a96ddbe3f6e2efc794f2befbfe247217479ef4b68030d42c3",
                     host_topic
@@ -37,13 +49,15 @@ impl TestApp {
     }
 
     pub fn log_test_app1_update(deal_id: &str) -> Value {
+        let test_app = Self::test_app1();
+        let app_cid = test_app.encoded_cid();
         json!(
               {
                 "address": deal_id,
                 "topics": [
                   "0x0e85c04920a2349be7d0f03a765fa172e5dabc0a4a9fc47acb81c07ce8d260d0",
                 ],
-                "data": "0x0155122000000000000000000000000000000000000000000000000000000000ae5c519332925f31f747a4edd958fb5b0791b10383ec6d5e77e2264f211e09e3",
+                "data": format!("0x{app_cid}"),
 
                 "blockNumber": "0x300",
                 "transactionHash": "0xb825edf7da59840ce838a9ed70aa0aa6c54c322ca5d6f0be4f070766e46ebbd8",
@@ -57,14 +71,14 @@ impl TestApp {
 
     pub fn test_app2() -> Self {
         Self {
-            cid: "bafkreicdwo6xrumiqc5a7oghbkay4tmmejlmokpweyut5uhe2tehsycvmu".to_string(),
+            cid: "bafkreieodklcdlpj5skykwctr466425y3f2jz7jukzj3dujlvk3ptwffim".to_string(),
             services_names: vec!["newService1".to_string()],
         }
     }
 
     pub fn log_test_app2(deal_id: &str, block: u32, host_topic: &str) -> Value {
-        // CID: bafkreicdwo6xrumiqc5a7oghbkay4tmmejlmokpweyut5uhe2tehsycvmu
-        // some default fcli app name: newService1
+        let test_app = Self::test_app2();
+        let app_cid = test_app.encoded_cid();
         json!(
             {
                 "removed": false,
@@ -74,7 +88,7 @@ impl TestApp {
                 "blockHash": "0x4e301dc22b7eb4bfd9c22865d36dfb68d4eb96a218f7b5f92c71760497e111ca",
                 "blockNumber": to_hex(block),
                 "address": "0x0f68c702dc151d07038fa40ab3ed1f9b8bac2981",
-                "data": format!("0x000000000000000000000000{deal_id}88924347d3eddcdaa6e6a3844bea08cfc8dae2d5b43d8c6fa35de5fd9ab6cc750000000000000000000000000000000000000000000000000000000000000103015512200000000000000000000000000000000000000000000000000000000043b3bd78d18880ba0fb8c70a818e4d8c2256c729f626293ed0e4d4c879605565"),
+                "data": format!("0x000000000000000000000000{deal_id}88924347d3eddcdaa6e6a3844bea08cfc8dae2d5b43d8c6fa35de5fd9ab6cc750000000000000000000000000000000000000000000000000000000000000103{app_cid}"),
                 "topics": [
                   "0x1c13422d2375fe8a96ddbe3f6e2efc794f2befbfe247217479ef4b68030d42c3",
                   host_topic
@@ -84,13 +98,15 @@ impl TestApp {
     }
 
     pub fn log_test_app2_update(deal_id: &str) -> Value {
+        let test_app = Self::test_app2();
+        let app_cid = test_app.encoded_cid();
         json!(
               {
                 "address": deal_id,
                 "topics": [
                   "0x0e85c04920a2349be7d0f03a765fa172e5dabc0a4a9fc47acb81c07ce8d260d0",
                 ],
-                "data": "0x015512200000000000000000000000000000000000000000000000000000000043b3bd78d18880ba0fb8c70a818e4d8c2256c729f626293ed0e4d4c879605565",
+                "data": format!("0x{app_cid}"),
                 "blockNumber": "0x300",
                 "transactionHash": "0xb825edf7da59840ce838a9ed70aa0aa6c54c322ca5d6f0be4f070766e46ebbd8",
                 "transactionIndex": "0xb",
