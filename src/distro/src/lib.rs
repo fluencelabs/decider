aeccebb5d95e4d159c319ebe95e7ef9384f9ee16
Alexey Proshutinskiy <alexey.prosh@fluence.one>
2024-03-16 21:42:47 +0200

    wip

--- src/distro/src/lib.rs
+++ src/distro/src/lib.rs
@@ -6,34 +6,8 @@ use serde_json::{json, Value as JValue};
 
 pub use build_info::PKG_VERSION as VERSION;
 
-const CONNECTOR: &'static [u8] = include_bytes!("../decider-spell/chain_connector.wasm");
-const CURL_ADAPTER: &'static [u8] = include_bytes!("../decider-spell/curl_adapter.wasm");
-const CONFIG: &'static [u8] = include_bytes!("../decider-spell/Config.toml");
-
-const DECIDER_SPELL: &'static str = include_str!("../decider-spell/poll.main.air");
-const WORKER_SPELL: &'static str = include_str!("../decider-spell/deal_spell.main.air");
-
 pub mod build_info {
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
-}
-
-pub struct DistrService {
-    pub name: &'static str,
-    pub config: &'static [u8],
-    pub modules: HashMap<&'static str, &'static [u8]>,
-}
-
-pub fn connector_service_modules() -> DistrService {
-    DistrService {
-        // The name is used by the decider, so we don't need to explicitly pass the service id of the connector service
-        // The name is taken from the config. Would be nice one day to automatically take it from the project itself.
-        name: "chain_connector",
-        config: CONFIG,
-        modules: hashmap! {
-            "chain_connector" => CONNECTOR,
-            "curl_adapter" => CURL_ADAPTER,
-        },
-    }
 }
 
 pub struct DistrSpell {
@@ -50,18 +24,6 @@ pub struct DeciderConfig {
     pub worker_ipfs_multiaddr: String,
     /// How often to run the worker-spell for updates/healthchecks
     pub worker_period_sec: u32,
-    /// URI of the RPC for decider to communicate with the chain (poll, send tx)
-    pub chain_api_endpoint: String,
-    /// The block number from which to poll new deals in hex format
-    pub chain_contract_block_hex: String,
-    /// Address of the Market contract
-    pub chain_matcher_addr: String,
-    /// How much gas is needed to register a worker
-    pub chain_workers_gas: u64,
-    /// Private key to send `setWorker` tx to chain to register a worker
-    pub chain_wallet_key: String,
-    // ID of the chain behind RPC. EIP-155: protection from replay on other chains.
-    pub chain_network_id: u64,
 }
 
 pub fn decider_spell(config: DeciderConfig) -> DistrSpell {
@@ -76,12 +38,7 @@ pub fn decider_spell(config: DeciderConfig) -> DistrSpell {
                 "script": WORKER_SPELL,
                 "config": worker_config,
                 "ipfs": config.worker_ipfs_multiaddr
-            }),
-            "from_block" => json!(config.chain_contract_block_hex),
-            "chain" => json!( {
-                "api_endpoint": config.chain_api_endpoint,
-                "market": config.chain_matcher_addr,
-            }),
+            })
         },
     }
 }
